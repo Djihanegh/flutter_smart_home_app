6e5ef608bd95d770240ca294e4848d02466f72cf
djihane Ghilani <djihaneghilani@gmail.com>
2022-10-02 14:06:16 +0100

    added size config

---
+++ lib/presentation/home/components/body.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+import '../../../utils/size_config.dart';
+
+class Body extends StatefulWidget {
+  const Body({Key? key}) : super(key: key);
+
+  @override
+  State<Body> createState() => _BodyState();
+}
+
+class _BodyState extends State<Body> {
+  @override
+  Widget build(BuildContext context) {
+    SizeConfig().init(context);
+
+    return const Scaffold(
+      body: Body(),
+    );
+  }
+}
--- lib/presentation/home/home_screen.dart
+++ lib/presentation/home/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:smart_home_app/presentation/home/components/body.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -7,6 +8,6 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Body();
   }
 }
