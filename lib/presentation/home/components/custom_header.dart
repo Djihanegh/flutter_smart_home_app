880d3c937b0236f27a4aa0215ed6c4cc7cf97f8c
djihane Ghilani <djihaneghilani@gmail.com>
2022-10-02 17:19:39 +0100

    created custom header

---
+++ lib/presentation/home/components/custom_header.dart
@@ -0,0 +1,26 @@
+import 'package:auto_size_text/auto_size_text.dart';
+import 'package:flutter/material.dart';
+
+import '../../../utils/app_style.dart';
+import '../../../utils/app_texts.dart';
+import 'add_button.dart';
+
+class CustomHeader extends StatelessWidget {
+  const CustomHeader({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        AutoSizeText(
+          welcomeText,
+          style: TextStyles.karla
+              .copyWith(color: Colors.black.withOpacity(0.8), fontSize: 25),
+        ),
+        const CustomAddButton()
+      ],
+    );
+  }
+}
---
+++ lib/utils/app_texts.dart
@@ -0,0 +1,1 @@
+const welcomeText = "Djihane Ghilani Home";
